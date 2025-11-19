c351f0ea8cf52219a8e790ce3e004569b9dd0268
Stas Steblyanskiy <ssteblyanskiy@bk.ru>
2025-11-19 14:17:30 +0300

    Tests, sonar, checkstyle and other

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,39 @@
 plugins {
-    id("java")
-    id("application")
+    application
+    checkstyle
+    jacoco
+    id("org.sonarqube") version "7.0.1.6134"
 }
 
 group = "hexlet.code"
 version = "1.0.0"
 
+checkstyle {
+    toolVersion = "10.12.4"
+    configFile = file("checkstyle.xml")
+    isIgnoreFailures = false
+    maxWarnings = 0
+}
+
+jacoco {
+    toolVersion = "0.8.14"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+    }
+}
+
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(25)
     }
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 repositories {
@@ -22,6 +46,23 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
+sonar {
+    properties {
+        property("sonar.projectKey", "SaintCap_java-project-78")
+        property("sonar.organization", "saintcap")
+    }
+}
+
+tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks {
+    checkstyleMain {
+        dependsOn(compileJava)
+    }
+
+    checkstyleTest {
+        dependsOn(compileTestJava)
+    }
 }
