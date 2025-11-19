cc25394f0650bc153600aa05014e4cad631c6296
Stas Steblyanskiy <ssteblyanskiy@bk.ru>
2025-11-19 13:58:31 +0300

    Initial project build

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "hexlet.code"
+version = "1.0.0"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,7 @@
+package hexlet.code;
+
+public class App {
+    static void main(String[] args) {
+        IO.println("Hello, world!");
+    }
+}
